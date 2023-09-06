45d1a6dd99624200b828d2d8222a686af91c48e4
James Lule <43647523+jlule@users.noreply.github.com>
2023-09-06 02:09:47 -0700

    mongo

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 // import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+// import Mongo from '../assets/mongo.png';
 import Angular from '../assets/angular.png';
 import Cplusplus from '../assets/c++.png';
 import Csharp from '../assets/c-sharp.png';
@@ -75,10 +75,10 @@ const Skills = () => {
                     <img className='w-20 mx-auto' src={Python} alt="HTML icon" />
                     <p className='my-4'>Python</p>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
                     <p className='my-4'>MONGODB</p>
-                </div>
+                </div> */}
             </div>
         </div>
       </div>
