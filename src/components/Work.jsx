d3a604775c48b1d1defb6a437372cf04bbba3f25
James Lule <43647523+jlule@users.noreply.github.com>
2023-09-08 17:36:05 -0700

    phpmotors

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from '../assets/workImg.jpeg'
 import realEstate from '../assets/realestate.jpg'
+import phpmotors from '../assets/phpmotors.png'
 
 const Work = () => {    
   return (
@@ -38,20 +39,20 @@ const Work = () => {
 
           </div>
           
-          <div style={{backgroundImage:`url(${realEstate})`}}
+          <div style={{backgroundImage:`url(${phpmotors})`}}
           className='shadow-lg shadow-[#040c161] group container rounded-md flex justify-center items-center mx-auto content-div'>
             
             {/* Hover  Effects*/}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application  
+                PHP Motors  
 
               </span>
               <div className='pt-8 text-center'> 
-                <a href="/">
+                <a href="https://www.youtube.com/watch?v=YkHXamLNrlA&ab_channel=michaeljames">
                   <button className='text-center rounded-lg px-4 py-3 m-3 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/jlule/PHP-motors-website/tree/main/phpmotors">
                   <button className='text-center rounded-lg px-4 py-3 m-3 bg-white text-gray-700 font-bold text-lg'>Code</button>
                 </a>
 
