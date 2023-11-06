32a536f1ad6e53cb84969f728fac971f6f87cb81
James Lule <43647523+jlule@users.noreply.github.com>
2023-11-05 19:35:18 -0800

    new commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,7 +8,7 @@ import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 // import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
-// import Mongo from '../assets/mongo.png';
+import Mongo from '../assets/mongo.png';
 import Angular from '../assets/angular.png';
 import Cplusplus from '../assets/c++.png';
 import Csharp from '../assets/c-sharp.png';
@@ -60,8 +60,8 @@ const Skills = () => {
                     <p className='my-4'>TAILWIND</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
-                    <p className='my-4'>FIREBASE</p>
+                    <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
+                    <p className='my-4'>MONGO-DB</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Csharp} alt="HTML icon" />
